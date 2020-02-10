fa77ade21e79bf3d6dba9f9ce0b3e133b88a3bfa
Jason Sholler <36761536+f1rsty@users.noreply.github.com>
2020-02-10 10:10:55 -0500

    Update systeminfocheck.bat

--- systeminfocheck.bat
+++ systeminfocheck.bat
@@ -2,6 +2,7 @@
 set LOGPATH=%SystemDrive%\Logs
 set LOGFILE=%COMPUTERNAME%_system_information.log
 set APPDIR=%APPDATA%\Nuance\NaturallySpeaking12
+set WMIC=%WINDIR%\system32\wbem\wmic.exe
 
 :::::::::::::::::::::
 :: PREP AND CHECKS ::
@@ -30,10 +31,6 @@ FOR /F "tokens=* USEBACKQ" %%F IN (`hostname`) DO (
 SET HOSTNAME=%%F
 )
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`wmic product where "Name like '%Dragon%'" get Name,Version`) DO (
-SET DRAGON_VERSION=%%F
-)
-
 title System Checker v%SCRIPT_VERSION% (%SCRIPT_UPDATED%)
 
 if not exist %LOGPATH% mkdir %LOGPATH%
@@ -44,7 +41,8 @@ call :log "%CUR_DATE% %TIME% Processor: %PROCESSOR_ARCHITECTURE%"
 call :log "%CUR_DATE% %TIME% Current User: %CURRENT_USER%"
 call :log "%CUR_DATE% %TIME% Hostname: %HOSTNAME%"
 call :log "%CUR_DATE% %TIME% Uptime: %UPTIME%"
-call :log "%CUR_DATE% %TIME% Dragon Version: %DRAGON_VERSION%"
+call :log "%CUR_DATE% %TIME% Dragon Version: "
+%WMIC% product where "Name like '%Dragon%'" get Name,Version
 call :log "%CUR_DATE% %TIME% Folder Permissions: "
 icacls %APPDIR% >> "%LOGPATH%\%LOGFILE%"
 
